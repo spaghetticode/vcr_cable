d4d09cc75f864b59bba3151a11eb09035a29ecb9
andrea longhi <andrea@spaghetticode.it>
2012-08-24 22:10:28 +0200

    use config/vcr_cable.yml for config

--- lib/vcr_cable.rb
+++ lib/vcr_cable.rb
@@ -2,20 +2,15 @@ require 'vcr'
 require 'vcr_cable/railtie' if defined? Rails
 
 module VcrCable
-  DEFAULT_CONFIG = {
-    :cassette_lib => "#{Rails.env}_cassettes",
-    :hook_into    => :fakeweb,
-    :allow_conn   => true,
-    :environments => [:development]
-  }
-
-  # allow to load config from a config file, or to change it via initializer...
-  # which is better?
-  def self.config_vcr
+  def self.configure_vcr
     VCR.configure do |c|
-      c.cassette_library_dir = DEFAULT_CONFIG[:cassette_lib]
-      c.hook_into DEFAULT_CONFIG[:hook_into]
-      c.allow_http_connections_when_no_cassette = DEFAULT_CONFIG[:allow_conn]
+      c.hook_into config['hook_into']
+      c.cassette_library_dir = config['cassette_library_dir']
+      c.allow_http_connections_when_no_cassette = config['allow_http_connections_when_no_cassette']
     end
+  end
+
+  def self.config
+    @config ||= YAML.load_file(Rails.root.join 'config/vcr_cable.yml')[Rails.env]
   end
 end
--- test/vcr_cable_test.rb
+++ test/vcr_cable_test.rb
@@ -1,14 +1,12 @@
 require 'test_helper'
 
 class VcrCableTest < ActiveSupport::TestCase
-  test 'it loads the default config' do
-    VcrCable.config_vcr
+  test 'loads the default config' do
+    VcrCable.configure_vcr
     assert_match /test_cassettes$/, VCR.configuration.cassette_library_dir
   end
 
   test 'adds VCR::Middleware::Rack to the middleware stack' do
-    Dir.chdir Rails.root do
-      assert `rake middleware`.include?('use VCR::Middleware::Rack')
-    end
+    assert Rails.configuration.middleware.any? {|name| name == 'VCR::Middleware::Rack'}
   end
 end
